81f08801a4a406531fd06f0db6fd086d3ba16526
Fabian Castaño <fabioc9675@gmail.com>
2021-07-14 18:30:24 -0500

    Addition of button and text with basic functionallity

--- SmartBedding_BLE/App.js
+++ SmartBedding_BLE/App.js
@@ -1,19 +1,68 @@
 import React, { Component, useState } from "react";
-import { SafeAreaView, StyleSheet, Text, View, StatusBar } from "react-native";
-import { BleManager } from "react-native-ble-plx";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Button,
+} from "react-native";
+import { BleManager, Base64 } from "react-native-ble-plx";
 
 export default class SB_BLE extends Component {
   // creation of constructor for SB_BLE
   constructor() {
     super();
     this.manager = new BleManager();
+    this.state = {
+      deviceid: "",
+      serviceUUID: "",
+      characteristicsUUID: "",
+      text1: "Hello BLE",
+      makedata: [],
+      showToast: false,
+      notificationReceiving: false,
+    };
+    console.log("App is running");
   }
+
+  changeTextFunction = () => {
+    this.setState({
+      text1: "Disconnect",
+      deviceid: "noNull",
+    });
+  };
+
+  // function for disconnect device
+  disconnect = () => {
+    console.log("disconnect");
+    this.setState({
+      deviceid: "",
+      text1: "Hola Fabian",
+    });
+  };
+
+  // function fos scan and connect device
+  scanAndConnect = () => {
+    console.log("scan and connect");
+    this.changeTextFunction();
+  };
 
   // creation of render function to put the components in the screen
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Hello SmartBedding</Text>
+        <View style={styles.bleContainer}>
+          {this.state.deviceid ? (
+            <Button title={"Disconnect"} onPress={() => this.disconnect()} />
+          ) : (
+            <Button title={"Scan"} onPress={() => this.scanAndConnect()} />
+          )}
+        </View>
+        <View style={styles.textBox}>
+          <Text>{this.state.text1}</Text>
+        </View>
         <StatusBar
           animated={true}
           backgroundColor="#61dafb"
@@ -37,5 +86,14 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     paddingTop: 25,
+  },
+  bleContainer: {
+    paddingTop: 30,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  textBox: {
+    alignItems: "center",
+    marginVertical: 10,
   },
 });
