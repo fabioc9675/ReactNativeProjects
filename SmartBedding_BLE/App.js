bb55fbbeb3e01736c26ca6598514e89cde2ea4e6
Fabian Castaño <fabioc9675@gmail.com>
2021-07-14 13:52:11 -0500

    modification of main structure of app to a class

--- SmartBedding_BLE/App.js
+++ SmartBedding_BLE/App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class SB_BLE extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello Fabian Castaño!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
