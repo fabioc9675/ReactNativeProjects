28bba2e894c3725ac8c95ce6891dc01338411634
Fabian <fabioc9675@gmail.com>
2021-10-05 01:52:06 -0500

    Creation of routes to communicate with database

--- mern-stack-task/src/routes/task.routes.js
+++ mern-stack-task/src/routes/task.routes.js
@@ -3,16 +3,43 @@ const router = express.Router();
 
 const Task = require("../models/task");
 
-router.get("/", (req, res) => {
+// reading data from database
+router.get("/", async (req, res) => {
   // make a request for the database
-  Task.find(function (err, tasks) {
-    console.log(tasks);
-  });
-
+  const tasks = await Task.find();
   // when client ask for '/' server response
-  res.json({
-    status: "API works!",
-  });
+  res.json(tasks);
+});
+
+// reading data by id
+router.get("/:id", async (req, res) => {
+  // make a request for the database
+  const task = await Task.findById(req.params.id);
+  // when client ask for '/' server response
+  res.json(task);
+});
+
+// adding data to the database
+router.post("/", async (req, res) => {
+  const { title, description } = req.body;
+  const task = new Task({ title, description });
+  console.log(task);
+  await task.save();
+  res.json({ status: "Task Recorded" });
+});
+
+// update data
+router.put("/:id", async (req, res) => {
+  const { title, description } = req.body;
+  const newTask = { title, description };
+  await Task.findByIdAndUpdate(req.params.id, newTask);
+  res.json({ status: "Task Modified" });
+});
+
+// delete data
+router.delete("/:id", async (req, res) => {
+  await Task.findByIdAndRemove(req.params.id);
+  res.json({ status: "Task Deleted" });
 });
 
 module.exports = router;
