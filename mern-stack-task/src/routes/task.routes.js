ee407a9fce45bf8a4af1c9cd40bb9204de31a0cb
Fabian <fabioc9675@gmail.com>
2021-10-05 01:10:25 -0500

    first request to the database

--- mern-stack-task/src/routes/task.routes.js
+++ mern-stack-task/src/routes/task.routes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const Task = require("../models/task");
+
 router.get("/", (req, res) => {
+  // make a request for the database
+  Task.find(function (err, tasks) {
+    console.log(tasks);
+  });
+
   // when client ask for '/' server response
   res.json({
     status: "API works!",
