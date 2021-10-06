aeaae51a99628c0ed6dd992eaeb9361f81ef69a5
Fabian Castaño <fabioc9675@gmail.com>
2021-10-06 01:33:50 -0500

    Addition of timestamp functionality

--- mern-stack-task/src/routes/task.routes.js
+++ mern-stack-task/src/routes/task.routes.js
@@ -12,9 +12,27 @@ router.get("/", async (req, res) => {
 });
 
 // reading data by id
-router.get("/:id", async (req, res) => {
+router.get("/id/:id", async (req, res) => {
   // make a request for the database
   const task = await Task.findById(req.params.id);
+  // when client ask for '/' server response
+  res.json(task);
+});
+
+// reading data by date
+router.get("/date/:start/:end", async (req, res) => {
+  // make a request for the database
+  // Examples
+  // http://localhost:3000/api/task/date/2021-10-05/2021-10-06T10:00:00Z
+  // http://localhost:3000/api/task/date/2021-10-06T06:00:00Z/2021-10-06T07:00:00Z
+  const start = new Date(req.params.start);
+  const end = new Date(req.params.end);
+  const task = await Task.find({
+    createdAt: {
+      $gte: start,
+      $lt: end,
+    },
+  });
   // when client ask for '/' server response
   res.json(task);
 });
@@ -29,7 +47,7 @@ router.post("/", async (req, res) => {
 });
 
 // update data
-router.put("/:id", async (req, res) => {
+router.put("/id/:id", async (req, res) => {
   const { title, description } = req.body;
   const newTask = { title, description };
   await Task.findByIdAndUpdate(req.params.id, newTask);
@@ -37,7 +55,7 @@ router.put("/:id", async (req, res) => {
 });
 
 // delete data
-router.delete("/:id", async (req, res) => {
+router.delete("/id/:id", async (req, res) => {
   await Task.findByIdAndRemove(req.params.id);
   res.json({ status: "Task Deleted" });
 });
