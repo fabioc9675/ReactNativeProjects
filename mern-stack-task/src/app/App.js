c58101cfe372f139aeae9b69f7257f654a7fc8d8
Fabian Castaño <fabioc9675@gmail.com>
2021-10-07 10:31:08 -0500

    Form creation with react

--- mern-stack-task/src/app/App.js
+++ mern-stack-task/src/app/App.js
@@ -4,7 +4,44 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>TESTING</h1>
+        {/* Navigationn */}
+        <nav className="light-blue darken-4">
+          <div className="container">
+            <a className="barnd-logo" href="/">
+              MERN Stack
+            </a>
+          </div>
+        </nav>
+
+        <div className="container">
+          <div className="row">
+            <div className="col 5s">
+              <div className="card">
+                <div className="card-content">
+                  <form>
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <input type="text" placeholder="Task Title"></input>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <textarea
+                          className="materialize-textarea"
+                          placeholder="Task Description"
+                        ></textarea>
+                      </div>
+                    </div>
+                    <button className="btn light-blue darken-4" type="submit">
+                      Send
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="col s7"></div>
+          </div>
+        </div>
       </div>
     );
   }
