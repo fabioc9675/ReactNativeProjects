11fa0ae6e3ff3c0390e4871ac29e255b3aca8f55
Fabian Castaño <fabioc9675@gmail.com>
2022-03-12 14:32:28 -0500

    Manage disconnection on database

--- React_Auth_SQL/server/db.js
+++ React_Auth_SQL/server/db.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+// use pool to mantain the connection
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+});
+
+// ... later
+connection.query("select 1 + 1", (err, rows) => {
+  /* */
 });
 
 module.exports = connection;
