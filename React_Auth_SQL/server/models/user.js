b14077350fa634b3d23c3845ab2d98c474191e0f
Fabian Castano <fabioc9675@gmail.com>
2022-03-19 23:48:28 -0500

    Implementation of validation in User model

--- React_Auth_SQL/server/models/user.js
+++ React_Auth_SQL/server/models/user.js
@@ -7,10 +7,93 @@ var User = database.define("USERs", {
     primaryKey: true,
     autoIncrement: true,
   },
-  USER_NAME: Sequelize.STRING,
-  USER_PASS: Sequelize.STRING,
+  USER_NAME: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: false,
+    unique: true,
+    validate: {
+      is: /^[a-z]+$/i, // matches this RegExp
+      notNull: true, // won't allow null
+      notEmpty: true, // don't allow empty strings
+      len: [5, 20], // only allow values with length between 2 and 10
+    },
+  },
+  USER_PASS: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: false,
+    validate: {
+      notNull: true, // won't allow null
+      notEmpty: true, // don't allow empty strings
+      len: [5, 20], // only allow values with length between 2 and 10
+    },
+  },
   USER_TOKEN: Sequelize.STRING,
-  USER_MAIL: Sequelize.STRING,
+  USER_MAIL: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
 });
 
 module.exports = User;
+
+/* 
+https://sequelize.org/master/manual/validations-and-constraints.html
+sequelize.define('foo', {
+  bar: {
+    type: DataTypes.STRING,
+    validate: {
+      is: /^[a-z]+$/i,          // matches this RegExp
+      is: ["^[a-z]+$",'i'],     // same as above, but constructing the RegExp from a string
+      not: /^[a-z]+$/i,         // does not match this RegExp
+      not: ["^[a-z]+$",'i'],    // same as above, but constructing the RegExp from a string
+      isEmail: true,            // checks for email format (foo@bar.com)
+      isUrl: true,              // checks for url format (http://foo.com)
+      isIP: true,               // checks for IPv4 (129.89.23.1) or IPv6 format
+      isIPv4: true,             // checks for IPv4 (129.89.23.1)
+      isIPv6: true,             // checks for IPv6 format
+      isAlpha: true,            // will only allow letters
+      isAlphanumeric: true,     // will only allow alphanumeric characters, so "_abc" will fail
+      isNumeric: true,          // will only allow numbers
+      isInt: true,              // checks for valid integers
+      isFloat: true,            // checks for valid floating point numbers
+      isDecimal: true,          // checks for any numbers
+      isLowercase: true,        // checks for lowercase
+      isUppercase: true,        // checks for uppercase
+      notNull: true,            // won't allow null
+      isNull: true,             // only allows null
+      notEmpty: true,           // don't allow empty strings
+      equals: 'specific value', // only allow a specific value
+      contains: 'foo',          // force specific substrings
+      notIn: [['foo', 'bar']],  // check the value is not one of these
+      isIn: [['foo', 'bar']],   // check the value is one of these
+      notContains: 'bar',       // don't allow specific substrings
+      len: [2,10],              // only allow values with length between 2 and 10
+      isUUID: 4,                // only allow uuids
+      isDate: true,             // only allow date strings
+      isAfter: "2011-11-05",    // only allow date strings after a specific date
+      isBefore: "2011-11-05",   // only allow date strings before a specific date
+      max: 23,                  // only allow values <= 23
+      min: 23,                  // only allow values >= 23
+      isCreditCard: true,       // check for valid credit card numbers
+
+      // Examples of custom validators:
+      isEven(value) {
+        if (parseInt(value) % 2 !== 0) {
+          throw new Error('Only even values are allowed!');
+        }
+      }
+      isGreaterThanOtherField(value) {
+        if (parseInt(value) <= parseInt(this.otherField)) {
+          throw new Error('Bar must be greater than otherField.');
+        }
+      }
+    }
+  }
+});
+*/
