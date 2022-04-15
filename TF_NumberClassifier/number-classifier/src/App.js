d2f753fa7bb633c213cd2eceeae874d61625fe7f
Fabian Castaño <fabioc9675@gmail.com>
2022-04-15 14:36:17 -0500

    Create server, separate resize function and load tf model

--- TF_NumberClassifier/number-classifier/src/App.js
+++ TF_NumberClassifier/number-classifier/src/App.js
@@ -1,4 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+import resample_single from "./resize";
+
+// Model URL
+const url = {
+  model: "http://localhost:8000/number-classifier/assets/model/model.json", // loaded from server runing on python localhost:8000
+};
 
 function App() {
   const canvasRef = useRef(null);
@@ -6,10 +13,28 @@ function App() {
   const contextRef = useRef(null);
   // const smallCtxRef = useRef(null);
 
+  const [model, setModel] = useState();
   const [isDrawing, setIsDrawing] = useState(false);
+  const [textOut, setTextOut] = useState("");
+
+  // function to load the model
+  async function loadModel(url) {
+    try {
+      const model = await tf.loadLayersModel(url.model);
+      setModel(model);
+      console.log("Model loaded");
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   // initialize canvas component
   useEffect(() => {
+    // Load Model
+    tf.ready().then(() => {
+      loadModel(url);
+    });
+
     const canvas = canvasRef.current;
     canvas.width = 500; // window.innerWidth*2
     canvas.height = 500; // window.innerHeight*2
@@ -71,91 +96,8 @@ function App() {
     resample_single(canvasRef.current, 28, 28, smallRef.current);
   };
 
-  const resample_single = (canvas, width, height, resize_canvas) => {
-    var width_source = canvas.width;
-    var height_source = canvas.height;
-    width = Math.round(width);
-    height = Math.round(height);
-
-    var ratio_w = width_source / width;
-    var ratio_h = height_source / height;
-    var ratio_w_half = Math.ceil(ratio_w / 2);
-    var ratio_h_half = Math.ceil(ratio_h / 2);
-
-    var ctx = canvas.getContext("2d");
-    var ctx2 = resize_canvas.getContext("2d");
-    var img = ctx.getImageData(0, 0, width_source, height_source);
-    var img2 = ctx2.createImageData(width, height);
-    var data = img.data;
-    var data2 = img2.data;
-
-    // function to resize
-    for (var j = 0; j < height; j++) {
-      for (var i = 0; i < width; i++) {
-        var x2 = (i + j * width) * 4;
-        var weight = 0;
-        var weights = 0;
-        var weights_alpha = 0;
-        var gx_r = 0;
-        var gx_g = 0;
-        var gx_b = 0;
-        var gx_a = 0;
-        var center_y = (j + 0.05) * ratio_h;
-        var yy_start = Math.floor(j * ratio_h);
-        var yy_stop = Math.ceil((j + 1) * ratio_h);
-        for (var yy = yy_start; yy < yy_stop; yy++) {
-          var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
-          var center_x = (i + 0.5) * ratio_w;
-          var w0 = dy * dy; // pre-calc part of w
-          var xx_start = Math.floor(i * ratio_w);
-          var xx_stop = Math.ceil((i + 1) * ratio_w);
-          for (var xx = xx_start; xx < xx_stop; xx++) {
-            var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
-            var w = Math.sqrt(w0 + dx * dx);
-            if (w >= 1) {
-              // pixel too far
-              continue;
-            }
-            // hermite filter
-            weight = 2 * w * w * w - 3 * w * w + 1;
-            var pos_x = 4 * (xx + yy * width_source);
-            // alpha
-            gx_a += weight * data[pos_x + 3];
-            weights_alpha += weight;
-            // colors
-            if (data[pos_x + 3] < 255) {
-              weight = (weight * data[pos_x + 3]) / 250;
-            }
-            gx_r += weight * data[pos_x];
-            gx_g += weight * data[pos_x + 1];
-            gx_b += weight * data[pos_x + 2];
-            weight += weight;
-          }
-        }
-        data2[x2] = gx_r / weights;
-        data2[x2 + 1] = gx_g / weights;
-        data2[x2 + 2] = gx_b / weights;
-        data2[x2 + 3] = gx_a / weights_alpha;
-      }
-    }
-
-    // Ya que esta, exagerarlo. Blancos blancos y Negros negros
-
-    for (var p = 0; p < data2.length; p += 4) {
-      var gris = data2[p]; // esta en blanco y negro
-
-      if (gris < 100) {
-        gris = 0;
-      } else {
-        gris = 255;
-      }
-
-      data2[p] = gris;
-      data2[p + 1] = gris;
-      data2[p + 2] = gris;
-    }
-
-    ctx2.putImageData(img2, 0, 0);
+  const predictHandle = () => {
+    console.log("prediction");
   };
 
   return (
@@ -174,6 +116,7 @@ function App() {
       </div>
       <button onClick={eraseHandle}>Borrar</button>
       <button onClick={copyHandle}>Copiar</button>
+      <button onClick={predictHandle}>Predict</button>
     </div>
   );
 }
