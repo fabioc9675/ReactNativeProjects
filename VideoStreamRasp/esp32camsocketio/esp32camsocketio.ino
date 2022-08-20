a31d983b8bcbcfe67511c1a3e475b02e68714d1c
Fabian Castano <fabioc9675@gmail.com>
2022-08-20 12:12:06 -0500

    Implementation of camera transmit with ESP32CAM, Websocket and Heroku

--- VideoStreamRasp/esp32camsocketio/esp32camsocketio.ino
+++ VideoStreamRasp/esp32camsocketio/esp32camsocketio.ino
@@ -1,12 +1,19 @@
-// Repo = https://github.com/Inglebard/esp32cam-relay
+// Repository Example: https://github.com/Inglebard/esp32cam-relay
 
+/* ********************************************************
+ * **** INCLUDE FILES *************************************
+ * ********************************************************/
 #include "WiFi.h"
 #include "esp_camera.h"
 
 #include "SocketIOclientMod.hpp"
-
 #include <ArduinoJson.h>
 
+#include <base64.h>
+
+/* ********************************************************
+ * **** DEFINES *******************************************
+ * ********************************************************/
 // Pin definition for CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM 32
 #define RESET_GPIO_NUM -1
@@ -28,13 +35,24 @@
 
 #define USE_SERIAL Serial
 
-// Replace with your network credentials
+/* ********************************************************
+ * **** CONSTANTS *****************************************
+ * ********************************************************/
+// Network credentials
 const char *hostname = "ESP32CAM";
 const char *ssid = "CLARO_WIFI93";
 const char *password = "Grezzo57";
 
+/* ********************************************************
+ * **** VARIABLES *****************************************
+ * ********************************************************/
+unsigned long messageTimestamp = 0;
+
 SocketIOclientMod socketIO;
 
+/* ********************************************************
+ * **** FUNCTIONS *****************************************
+ * ********************************************************/
 void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length)
 {
     switch (type)
@@ -68,7 +86,6 @@ void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length)
 
 void setupCamera()
 {
-
     camera_config_t config;
     config.ledc_channel = LEDC_CHANNEL_0;
     config.ledc_timer = LEDC_TIMER_0;
@@ -104,11 +121,16 @@ void setupCamera()
     }
 }
 
+/* ********************************************************
+ * **** MAIN FUNCTION *************************************
+ * ********************************************************/
 void setup()
 {
     Serial.begin(115200);
 
-    // Connect to Wi-Fi
+    Serial.println("Program begin...");
+
+    // Connect to the WiFi
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED)
     {
@@ -119,29 +141,35 @@ void setup()
     // Print ESP32 Local IP Address
     Serial.println(WiFi.localIP());
 
+    // Setup camera
     setupCamera();
 
     // server address, port and URL
-    socketIO.begin("192.168.20.3", 5000, "/socket.io/?EIO=4");
+    socketIO.begin("greenhouse-vid.herokuapp.com", 80, "/socket.io/?EIO=4"); // without https://
+    // socketIO.begin("192.168.20.3", 5000, "/socket.io/?EIO=4");
 
     // event handler
     socketIO.onEvent(socketIOEvent);
+
+    // try ever 5000 again if connection has failed
+    // socketIO.setReconnectInterval(5000);
 }
 
-unsigned long messageTimestamp = 0;
 void loop()
 {
     socketIO.loop();
+
     uint64_t now = millis();
 
-    // can't make it fast. SocketIO keep disconnecting
-    if (now - messageTimestamp > 300)
+    // sample frame
+    if (now - messageTimestamp > 500)
     {
         messageTimestamp = now;
 
-        camera_fb_t *fb = NULL;
+        Serial.println("Taking photo");
 
-        // Take Picture with Camera
+        // Taking Photo
+        camera_fb_t *fb = NULL;
         fb = esp_camera_fb_get();
         if (!fb)
         {
@@ -149,8 +177,56 @@ void loop()
             return;
         }
 
-        socketIO.sendBIN(fb->buf, fb->len);
+        // Serial.print("Buffer = ");
+        // Serial.println(fb->buf);
+
+        Serial.print("Size = ");
+        Serial.println(fb->len);
+
+        Serial.print("Format = ");
+        Serial.println(fb->format);
+
+        Serial.print("Height = ");
+        Serial.println(fb->height);
+
+        Serial.print("Width = ");
+        Serial.println(fb->width);
+
+        // Serial.print("Timestamp = ");
+        // Serial.println(fb->timestamp);
+
+        // String toEncode = "Hello World";
+        String encoded = "data:image/jpg;base64," + base64::encode(fb->buf, fb->len);
+
+        // creat JSON message for Socket.IO (event)
+        DynamicJsonDocument doc(65535);
+        JsonArray array = doc.to<JsonArray>();
+
+        // add evnet name
+        // Hint: socket.on('event_name', ....
+        array.add("stream");
+        array.add(encoded);
+
+        // add payload (parameters) for the event
+        // JsonObject param1 = array..createNestedObject();
+        // param1["stream"] = encoded;
+
+        // JSON to String (serializion)
+        String output;
+        serializeJson(doc, output);
+
+        // Send event
+        socketIO.sendEVENT(output);
+        // socketIO.sendBIN(fb->buf, fb->len);
+
+        // Print JSON for debugging
+        // USE_SERIAL.println(output);
+
+        // Serial.println(encoded);
+
+        // socketIO.sendBIN(fb->buf, fb->len);
         Serial.println("Image sent");
+
         esp_camera_fb_return(fb);
     }
 }
