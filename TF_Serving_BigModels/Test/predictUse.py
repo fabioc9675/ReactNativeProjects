43e9fac919a89ab1ab01d54725a3401d6689bfec
Fabian Castano <fabioc9675@gmail.com>
2022-09-03 22:45:05 -0500

    Implementation of tensorflow serving

---
+++ TF_Serving_BigModels/Test/predictUse.py
@@ -0,0 +1,6 @@
+import requests
+
+res = requests.post("http://localhost:8601/v1/models/email_model:predict", json={"instances": [
+                    "Let's meet for dinner tomorrow", "You are awarded a SiPix Digital Camera! call 09061221061 for landline. Delivery within 28 days. T Cs Box177"]})
+
+print(res.text)
